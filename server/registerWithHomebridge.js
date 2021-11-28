a553995cd4624372be36fc8a400763bcfaca0e4d
Peter Loveland <peter.loveland@infosum.com>
2021-11-28 11:59:19 +0000

    a

--- server/registerWithHomebridge.js
+++ server/registerWithHomebridge.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 if (process.env.NODE_ENV === "development") {
   global.homebridgeConfigPath = "/Volumes/pi's home/.homebridge/config.json"
 } else {
-  global.homebridgeConfigPath = "home/.homebridge/config.json"
+  global.homebridgeConfigPath = ".homebridge/config.json"
 }
 
 let homebridgeConfig = JSON.parse(fs.readFileSync(homebridgeConfigPath));
